00ee1ff1df1576b43dafbd292f74dc208a735e5c
Rangga Prasetya <ranggahd321@gmai.com>
2025-09-17 13:47:16 +0700

    Initial commit: streaming-film project

---
+++ client/src/pages/MyBookings.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mybookings = () => {
+    return (
+        <div>
+            <h1> MyBookings</h1>
+        </div>
+    )
+}
+
+export default Mybookings;
---
+++ client/src/pages/SeatLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SeatLayout = () => {
+    return (
+        <div>
+            <h1>Seat layout Page</h1>
+        </div>
+    )
+}
+
+export default SeatLayout;
