595f8d0d8758476301bdf8d9821a4e7269892bfc
ranggalabs <rangga123@gmail.com>
2025-10-04 18:04:59 +0700

    menambahkan fitur listbooking dan listshow

--- client/src/lib/dateFormat.js
+++ client/src/lib/dateFormat.js
@@ -7,3 +7,5 @@ export const dateFormat = (date) => {
         minute:'numeric'
     })
 }
+
+
--- client/src/pages/admin/ListBooking.jsx
+++ client/src/pages/admin/ListBooking.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyBookingData } from '../../assets/assets';
+import Loading from '../../components/Loading';
+import Title from '../../components/admin/Title';
 
 const ListBooking = () => {
-  return (
-    <div>
-      <h1>contoh</h1>
+  const currency = import.meta.env.VITE_CURRENCY
+  const [bookings, setBookings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getAllBookings = async () => {
+    setBookings(dummyBookingData)
+    setIsLoading(false);
+  }
+  useEffect(()=>{
+    getAllBookings();
+  }, []);
+
+  return !isLoading ? (
+    <>
+    <Title text1="List" text2="Bookings"/>
+    <div className='max-w-4xl mt-6 overflow-x-auto'>
+    <table className='w-full border-collapse rounded-md overflow-hidden text-nowrap'>
+      <thead>
+        <tr className='bg-primary/20 text-left text-white'>
+          <th className='p-2 font-medium pl-5'>User Name</th>
+          <th className='p-2 font-medium'>Movie Name</th>
+          <th className='p-2 font-medium'> Show Time</th>
+          <th className='p-2 font-medium'>Seats</th>
+          <th className='p-2 font-medium'>Amounts</th>
+        </tr>
+      </thead>
+    </table>
     </div>
-  )
+    </>
+  ) : <Loading/>
 }
 
 export default ListBooking
