7a1d0d72c933952718942273a2c04d3df0a4b9da
ranggalabs <rangga123@gmail.com>
2025-10-16 20:44:03 +0700

    update fix config bug

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,7 @@
 import { Inngest } from "inngest";
-import User from "../configs/models/User";
+import User from '../models/User.js';
+
+
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
@@ -12,7 +14,7 @@ const syncUserCreation = inngest.createFunction(
         const {id, first_name, last_name, email_addresses, image_url} = event.data
         const userData = {
             _id: id,
-            email: email_addresses[0].email_addresses,
+            email: email_addresses[0].email_address,
             name: first_name + ''+ last_name,
             image: image_url
         }
